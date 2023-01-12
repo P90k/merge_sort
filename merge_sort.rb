5806cd356bfa54bc780703c5bb5ec21a5764ad04
Andreas Jansson <andreas.jansson234@gmail.com>
2023-01-12 11:34:21 +0100

    Clean up code and remove unnecessary variables.

--- merge_sort.rb
+++ merge_sort.rb
@@ -6,22 +6,12 @@ def merge_sort(arr)
 
   left_arr =  merge_sort(arr[0...arr.length / 2])
   right_arr = merge_sort(arr[arr.length / 2..arr.length])
-  a = 0
-  b = 0
-  while a < left_arr.length && b < right_arr.length
-    if left_arr[a] < right_arr[b]
-      sorted_arr << left_arr[a]
-      a += 1
-    else
-      sorted_arr << right_arr[b]
-      b += 1
-    end
+
+  until left_arr.empty? || right_arr.empty?
+    sorted_arr << (left_arr[0] < right_arr[0] ? right_arr.shift : left_arr.shift)
+
   end
-  if a >= left_arr.length
-    sorted_arr.concat(right_arr[b..right_arr.length])
-  else
-    sorted_arr.concat(left_arr[a..left_arr.length])
-  end
+  left_arr.empty? ? sorted_arr.concat(right_arr) : sorted_arr.concat(left_arr)
 
   sorted_arr
 end
